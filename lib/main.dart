c361c7877e85eefc037fb7f3b65403aa788229a7
Byron Steven Toledo Medina <byron.toledo@bam.com.gt>
2026-05-07 09:49:11 -0600

    tu mensaje

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'screens/onboarding_screen.dart';
+import 'core/navigation/router.dart';
 
 void main() {
   runApp(const ProviderScope(child: MainApp()));
@@ -11,9 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: OnboardingScreen(),
+      routerConfig: router,
     );
   }
 }
